8415ca3e0597d39265632f28b1796f82b903b3a7
dordory <dordory@gmail.com>
2025-06-16 00:01:40 +0900

    구역데이터 업로드 기능 개선

--- apps/territory/admin_views.py
+++ apps/territory/admin_views.py
@@ -2,6 +2,7 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.db.utils import IntegrityError
 from .forms import TerritoryCSVUploadForm, VisitHistoryCSVUploadForm, CongregationCSVUploadForm
 import csv, io
 from datetime import datetime
@@ -48,6 +49,8 @@ def territory_upload_view(request):
             decoded_file = csv_file.read().decode("utf-8")
             is_string = io.StringIO(decoded_file)
 
+            territory_duplication_count = 0
+            territory_uploaded_count = 0
             reader = csv.DictReader(is_string)
             for row in reader:
                 try:
@@ -65,9 +68,15 @@ def territory_upload_view(request):
                         category=category,
                         note=row["note"],
                     )
+                    territory_uploaded_count += 1
+                except IntegrityError as e:
+                    territory_duplication_count += 1
                 except Exception as e:
                     messages.error(request, f"엘러 발생: {e}")
+            if territory_duplication_count:
+                messages.warning(request, f"{territory_duplication_count}건의 구역데이터는 이미 존재하고 있습니다.")
             messages.success(request, 'CSV 업로드가 성공적으로 완료되었습니다.')
+            messages.success(request, f"{territory_uploaded_count}건의 구역데이터가 추가 되었습니다.")
             return redirect('admin:territory_territory_changelist')
     else:
         form = TerritoryCSVUploadForm()
@@ -85,24 +94,33 @@ def visit_history_upload_view(request):
             decoded_file = csv_file.read().decode("utf-8")
             reader = csv.DictReader(io.StringIO(decoded_file))
 
+            history_duplication_count = 0
+            history_uploaded_count = 0
             for row in reader:
                 try:
                     territory = Territory.objects.get(code=row["territory_code"])
                     visitor = Member.objects.get(name=row["visitor"])
                     status = VisitStatus.objects.get(label=row["status"])
-                    date_str = row["visited_date"] + " " + row["visited_time"]
+                    date_str = row["visited_date"] + " " + (row["visited_time"] or "12:00:00")
                     visited_at = datetime.strptime(date_str, "%Y/%m/%d %H:%M:%S")
 
-                    VisitHistory.objects.update_or_create(
+                    VisitHistory.objects.create(
                         territory=territory,
                         visited_at=visited_at,
                         visitor=visitor,
                         status=status,
                     )
+                    history_uploaded_count += 1
+                except IntegrityError as e:
+                    history_duplication_count += 1
+                    pass
                 except Exception as e:
                     messages.error(request, f"에러 발생: {territory} {e}")
                 
+            if history_uploaded_count:
+                messages.warning(request, f"{history_duplication_count}건의 방문 데이터가 중복되었습니다.")
             messages.success(request, "VisitHistory CSV 업로드 성공")
+            messages.success(request, f"{history_uploaded_count}건의 방문 데이터가 업로드 되었습니다.")
             return redirect('admin:territory_visithistory_changelist')
     else:
         form = VisitHistoryCSVUploadForm()
